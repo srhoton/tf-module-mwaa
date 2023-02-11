0e406e59a7632c5d267f7a0bc5331651e8ab2367
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-11 12:11:37 -0500

    Initial checkin

---
+++ iam.tf
@@ -0,0 +1,104 @@
+resource "aws_iam_policy" "mwaa_execution_policy" {
+  name = "${var.env_name}-mwaa-execution-policy"
+  path = "/"
+  description = "Policy for MWAA execution role"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "airflow:PublishMetrics",
+            "Resource": "arn:aws:airflow:{$var.region}:{$var.account_id}:environment/{var.env_name}"
+        },
+        { 
+            "Effect": "Deny",
+            "Action": "s3:ListAllMyBuckets",
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.mwaa_bucket.id}",
+                "arn:aws:s3:::${aws_s3_bucket.mwaa_bucket.id}/*"
+            ]
+        },
+        { 
+            "Effect": "Allow",
+            "Action": [ 
+                "s3:GetObject*",
+                "s3:GetBucket*",
+                "s3:List*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.mwaa_bucket.id}",
+                "arn:aws:s3:::${aws_s3_bucket.mwaa_bucket.id}/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:GetLogRecord",
+                "logs:GetLogGroupFields",
+                "logs:GetQueryResults"
+            ],
+            "Resource": [
+                "arn:aws:logs:${var.region}:${var.account_id}:log-group:airflow-${var.env_name}-*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:DescribeLogGroups"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetAccountPublicAccessBlock"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:PutMetricData",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sqs:ChangeMessageVisibility",
+                "sqs:DeleteMessage",
+                "sqs:GetQueueAttributes",
+                "sqs:GetQueueUrl",
+                "sqs:ReceiveMessage",
+                "sqs:SendMessage"
+            ],
+            "Resource": "arn:aws:sqs:${var.region}:*:airflow-celery-*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt",
+                "kms:DescribeKey",
+                "kms:GenerateDataKey*",
+                "kms:Encrypt"
+            ],
+            "NotResource": "arn:aws:kms:*:${var.account_id}:key/*",
+            "Condition": {
+                "StringLike": {
+                    "kms:ViaService": [
+                        "sqs.${var.region}.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
+}  
+EOF
+}
