f4599a9a660b1a3eedc1b814d2b82fb987d30237
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-12 06:55:59 -0500

    change bucket output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "execution_role_arn" {
   value = aws_iam_role.mwaa_execution_role.arn
 }
 
-output "s3_arn" {
- value = aws_s3_bucket.mwaa_bucket.arn 
+output "s3_bucket_name" {
+ value = aws_s3_bucket.mwaa_bucket.id
 }
